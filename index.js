57326e7a90e85687f48f105efa679348d5d23a10
samanthamarberger <slatham04@gmail.com>
2022-09-22 15:09:08 -0600

    commit tree f4f7dcfc7280e24f36d27d0da08611e93ed39ce7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const arrayOfThankYou = [];
+function writeCards(names, event) {
+    for(let i = 0; i < names.length; i++) {
+        let thankYou = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        arrayOfThankYou.push(thankYou);
+    }
+    return arrayOfThankYou;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+
+}
